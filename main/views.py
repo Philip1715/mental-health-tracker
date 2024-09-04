e898ae15007c0cc24838f845e74d931301f50726
Philip1715 <philiphalomoan17@gmail.com>
2024-09-04 15:43:58 +0700

    server sudah run

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2306240130',
+        'name': 'Philip Halomoan Sampenta Manurung',
+        'class': 'PBP E'
+    }
+
+    return render(request, "main.html", context)

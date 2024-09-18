4ecf4aaafb0ef088f725b3414df72eeddfb0c3ef
Philip1715 <philiphalomoan17@gmail.com>
2024-09-18 14:29:32 +0700

    tutor3 menyalla tapi kok gabisa logout hmmmmm

--- mental_health_tracker/mental_health_tracker/settings.py
+++ mental_health_tracker/mental_health_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^n*m%fpsje+d39gkm079(8p$cpk&xl7ia=1ga4!jew^pk&t#87'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 ALLOWED_HOSTS = []
 
 

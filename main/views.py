1553ad282e956eefbaba30a88bcc8487537c3e4b
Philip1715 <philiphalomoan17@gmail.com>
2024-10-08 20:53:49 +0700

    tutor 5 done

--- main/views.py
+++ main/views.py
@@ -42,21 +42,17 @@ def create_mood_entry(request):
     return render(request, "create_mood_entry.html", context)
 def show_xml(request):
     data = MoodEntry.objects.filter(user=request.user)
-    data = MoodEntry.objects.all()
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
     data = MoodEntry.objects.filter(user=request.user)
-    data = MoodEntry.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
-    data = MoodEntry.objects.filter(user=request.user)
     data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json_by_id(request, id):
-    data = MoodEntry.objects.filter(user=request.user)
     data = MoodEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
